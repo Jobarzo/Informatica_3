2552440dcce63f5c582234934520f2a5ead54ffa
Jobarzo <davidevallati2000@hotmail.com>
2016-11-27 11:51:38 +0100

    E4_Biglietto_mirabilandia.c

--- Biglietto_mirabilandia.c
+++ Biglietto_mirabilandia.c
@@ -1,35 +1,57 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<MATH.H>
 
 /* "Biglietto_Mirabilandia"
- Autore:Davide Vallati;Classe:3°INA;Data:23/11/2016;Versione:2.0 */
+ Autore:Davide Vallati;Classe:3°INA;Data:23/11/2016;Versione:3.0 */
  
  main()
  {
- 	char Risposta;                                      //variabile che contiene la risposta
- 	char S;                                             //variabile che contiene il si
- 	char N;                                             //variabile che contiene il no
- 	int Eta;                                            //variabile che contiene l'età
- 	float Altezza;                                      //variabile che contiene l'altezza
- 	float Prezzo;                                       //variabile che contiene il prezzo
- 	printf("La persona è disabile? (S=si,N=no)"); 
- 	scanf("%c",&Risposta);                          
- 	if (Risposta==S){
- 		Prezzo=0;
-	 }
-	 else printf("Inserire Eta");                       
-	      scanf("%d",&Eta);                             
-	      if (Eta>=60){
-	      	Prezzo=25,00;
-		  }
-		  else printf("Inserire Altezza");              
-		       scanf("%f",&Altezza);                    
-		       if (Altezza<=100){
-		       	Prezzo=0;
-			   }
-			   else if (Altezza>=100 && Altezza<=140){
-			   	Prezzo=28,00;
-			   }
-			   else Prezzo=34,90;			   
- }
+ 	char Risposta;
+ 	char risposta;
+        int E;                                            
+ 	int H;                                     
+ 	float P;                                       
+ 	printf(" \" La persona e disabile? \" (1=si,0=no): \n   ");
+	scanf("%d",&Risposta);                                    
+	if(Risposta==1)
+	{
+	    P=0.00;
+	}
+	else{
+		printf(" \" E un accompagnatore? \" (1=si,0=no): \n   ");                             
+                scanf("%d",&risposta);
+		if(risposta==1)
+		{
+			P=25.50;
+		}
+		else{ 
+	             printf(" \"Inserire Eta\":\n   ");                       
+	             scanf("%d",&E);
+	             if (E>=60)
+		     {
+	      	             P=25.00;
+		     }
+		     else{ 
+		          printf(" \"Inserire Altezza \":\n   ");              
+		          scanf("%d",&H);                    
+		          if (H<=100)
+			  {
+		          	P=0.00;
+			  }
+			   else {
+			        if (H>=100 && H<=140)
+				{
+			   	     P=28.00;
+			        }
+			        else {
+			              P=34.90;
+				     }
+                                }
+		          } 			   
+                    }
+             }
+printf(" \"il prezzo del biglietto e \": \n   %f",P);
+}
+      
+
+		
+	
