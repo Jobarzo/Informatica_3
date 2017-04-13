23a237b8fffa65684b0f6baf98ee48d77791bb34
Jobarzo <davidevallati2000@hotmail.com>
2017-04-13 10:22:51 +0200

    Update E9_Tabellina_numeri_da_2_a_10.c

--- E9_Tabellina_numeri_da_2_a_10.c
+++ E9_Tabellina_numeri_da_2_a_10.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 
+
+/* Stampare le tabelline di tutti i numeri compresi tra 2 e 10
+   Autore: Davide Vallati - Classe: 3° INA - Data: 18/12/2016 - Versione: 1.0 */
+
 int main()
 {
     int I; //variabile che contiene il valore di I
     int n; //variabile che contiene il valore di n
     int T; //variabile che contiene T
     n=2;
-    while(n<=10){                                            //mentre n<=10 allora...
+    while(n<=10){ //mentre n<=10 allora...
     	         I=1;
     	         T=0;
-                 while(I<=10){                               //mentre I<=10 allora...
+                 while(I<=10){ //mentre I<=10 allora...
                               T=T+n;                                  
                               printf("%d ",T);                        
                               I++;
