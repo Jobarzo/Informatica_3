b92b847157b76ad48192e749a3e198cadbf913a5
Jobarzo <davidevallati2000@hotmail.com>
2017-09-08 11:32:54 +0200

    Add files via upload

---
+++ E17_Inserimento_elemento_array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+/* Inserimento di un elemento in un array
+   Autore: Davide Vallati - Classe: 3° INA - Data: 03/01/2017 - Versione: 1.0 */
+
+int main(){
+	int n;
+	int tot;
+	int max;
+	int trovato;
+	int I
+	
+	trovato=0;
+	tot=5;
+	max=10;
+	printf(" Inserisci il numero nell': ");
+	scanf("%d",&numeri[I]);
+	if(tot<max){
+		I=0;
+		while((I<tot)&&(trovato=0)){
+			if(n<=num[I]){
+				I++;
+			}
+			else{
+				trovato=1;
+			}
+		}
+	}
+}
